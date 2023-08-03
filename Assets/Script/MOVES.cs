2852daf4fc57acbe92e06554633853303686c4d8
SaltFish <4A9K0100@office.stust.edu.tw>
2023-08-03 15:48:35 +0800

    2

---
+++ Assets/Script/MOVES.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVES : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up")) { transform.Translate(0, 0.1f, 0); }
+        // 按住 上鍵 時，物件每個 frame 朝自身 z 軸方向移動 0.1 公尺
+
+        if (Input.GetKey("down")) { transform.Translate(0, -0.1f,0 ); }
+        // 按住 下鍵 時，物件每個 frame 朝自身 z 軸方向移動 -0.1 公尺
+
+
+    }
+}
